744f6d22cbec681f84d1765c2131e2c255a26b78
Stefano <stefano.armenes@gmail.com>
2017-02-25 10:21:21 +0100

    ora spinner added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const emoji = require('node-emoji');
 const fetch = require('node-fetch');
 const chalk = require('chalk');
 const Table = require('cli-table2');
+const ora = require('ora');
 
 const NEW_STORIES_URL = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty';
 const ITEM_URL = (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
@@ -20,6 +21,8 @@ const getStories = (ids) => {
 
 (async () => {
 
+  const spinner = ora({text: 'Loading your stories', color: 'green'}).start();
+
   try {
     const ids = await getNewStoriesIds();
     const stories = await Promise.all(getStories(ids));
@@ -27,9 +30,11 @@ const getStories = (ids) => {
       .filter(story => story !== null && story.url && SEARCH_WORDS.some(word => story.title.toLowerCase().includes(word)))
       .map(story => ([story.title, story.url ]));
 
+    spinner.stop();
+
     const table = new Table({
       head: [ 'Title', 'Url' ],
-      colWidths: [ 100, 100 ]
+      colWidths: [ 75, 100 ]
     });
 
     table.push(...filteredStories);
