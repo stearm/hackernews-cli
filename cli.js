f0bb92dbae8d9f836e332cd20979a92f17e7f21a
Stefano <stefano.armenes@gmail.com>
2017-02-26 11:31:51 +0100

    --collections option

--- cli.js
+++ cli.js
@@ -2,13 +2,20 @@ const program = require('commander');
 
 const { print } = require('./index.js');
 
+const list = list => list.split(',');
+
 program
   .command('stories')
   .description('Read last hacker news stories')
-  .option('-k, --keywords <keywords>', 'Keywords of your interest (comma separated)', list => list.split(','), [])
+  .option('-k, --keywords <keywords>', 'Keywords of your interest (comma separated)', list, [])
+  .option('-c, --collections <collections>', 'Collections of keywords in your home directory', list, [])
   .action(command => {
     if (command.keywords) {
       print(command.keywords)
+    }
+
+    if(command.collections) {
+      // TODO
     }
   });
 
