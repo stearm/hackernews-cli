0bf70e22a0cecd6888a756dffc9faddb46bb770d
Stefano <stefano.armenes@gmail.com>
2017-03-04 10:32:35 +0100

    fix cols length

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ora = require('ora');
 
 const NEW_STORIES_URL = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty';
 const ITEM_URL = (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
+const HEADER = chalk.green.bold(`===== ${emoji.get('space_invader')}  ===== Hacker news stories ===== ${emoji.get('space_invader')}  ======`);
 
 const getNewStoriesIds = async () => {
   const response = await fetch(NEW_STORIES_URL);
@@ -27,23 +28,29 @@ exports.print = async keywords => {
     const stories = await Promise.all(getStories(ids));
     const filteredStories = stories
       .filter(story => {
-        if (story === null && !story.url) return false;
+        if (story === null || !story.url) return false;
         if (!keywords.length) return true;
         return keywords.some(word => story.title.toLowerCase().includes(word));
       })
       .map(story => ([ story.title, story.url ]));
+
+    const maxTitleUrl = Math.max(...filteredStories.map(story => story[0].length));
+    const maxLengthUrl = Math.max(...filteredStories.map(story => story[1].length));
 
     spinner.stop();
 
     if (filteredStories.length) {
       const table = new Table({
         head: [ 'Title', 'Url' ],
-        colWidths: [ 75, 100 ]
+        colWidths: [ maxTitleUrl + 3, maxLengthUrl + 3 ],
+        style: {
+          head: ['green']
+        }
       });
 
       table.push(...filteredStories);
 
-      console.log(chalk.green.bold(`===== ${emoji.get('space_invader')}  ===== Hacker news stories ===== ${emoji.get('space_invader')}  ======`));
+      console.log(HEADER);
       console.log(table.toString());
     } else {
       console.log(chalk.green.bold(`No stories found ${emoji.get('disappointed')}`))
